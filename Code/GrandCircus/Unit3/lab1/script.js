3a9c3c2190dc78198c837fa7a3a38c60271e6988
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2023-10-19 21:53:53 -0400

    unit3

---
+++ Code/GrandCircus/Unit3/lab1/script.js
@@ -0,0 +1,27 @@
+const firstName = "Juan";
+const lastName = "Linares Orihuela";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Setubal, Portugal is my favorite city.
+const population = 121185;
+
+console.log(population); 
+console.log(population * 3);
+
+//Boolean to true
+const javaScriptIsCool = true;
+
+//Rainbow colors
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+//Loop that counts from 1 to 5 and logs each number to the console.
+for (let i = 1; i <=5; i++) {
+    console.log(i);
+}
+
+//Loop that counts from 0 to 4 and logs each number to the console.
+for (let i = 0; i <=4; i++) {
+    console.log(i);
+}
